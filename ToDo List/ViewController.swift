56be262912087ab504d66287e4407d72ae5d84f0
Denis <mrdunacdtm@gmail.com>
2022-01-08 01:28:55 +0900

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by 13 Denis on 08/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
