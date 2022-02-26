9da09909b71e0a7607be8a4f7c1d39bcb24ad10b
Denis <mrdunacdtm@gmail.com>
2022-02-26 18:52:02 +0900

    Worked on AVFoundation framework and added Camera View

---
+++ ToDo List/camera/CameraViewController.swift
@@ -0,0 +1,203 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by 13 Denis on 24/02/2022.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var shutterBtnUI: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var capturePhotoOutput: AVCapturePhotoOutput?
+    var qrCodeFrameView: UIView?
+    
+    var capturePhotoSettings: AVCapturePhotoSettings?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video as the media type parameter
+        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera , for: AVMediaType.video, position: .front) else {
+            fatalError("No video device found")
+        }
+        
+        
+        do {
+            
+            // Get an instance of the AVCaptureDeviceInput class using the previous deivce object
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Initialize the captureSession object
+            captureSession = AVCaptureSession()
+            
+            // Set the input devcie on the capture session
+            captureSession?.addInput(input)
+            
+            // Get an instance of ACCapturePhotoOutput class
+            capturePhotoOutput = AVCapturePhotoOutput()
+            capturePhotoOutput?.isHighResolutionCaptureEnabled = true
+            
+            
+            // Set the output on the capture session
+            captureSession?.addOutput(capturePhotoOutput!)
+            
+            // Initialize a AVCaptureMetadataOutput object and set it as the input device
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            
+            //Initialise the video preview layer and add it as a sublayer to the viewPreview view's layer
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            cameraView.layer.addSublayer(videoPreviewLayer!)
+            
+            //start video capture
+            captureSession?.startRunning()
+            
+            
+            
+            //Initialize QR Code Frame to highlight the QR code
+            qrCodeFrameView = UIView()
+            
+            if let qrCodeFrameView = qrCodeFrameView {
+                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+                qrCodeFrameView.layer.borderWidth = 2
+                view.addSubview(qrCodeFrameView)
+                view.bringSubviewToFront(qrCodeFrameView)
+            }
+        } catch {
+            //If any error occurs, simply print it out
+            print(error)
+            return
+        }
+        
+    }
+    
+    
+    
+    override func viewDidLayoutSubviews() {
+        videoPreviewLayer?.frame = view.bounds
+        if let previewLayer = videoPreviewLayer ,(previewLayer.connection?.isVideoOrientationSupported)! {
+            previewLayer.connection?.videoOrientation = UIApplication.shared.statusBarOrientation.videoOrientation ?? .portrait
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+    @IBAction func shutterBtn_Action(_ sender: Any) {
+        
+        // Make sure capturePhotoOutput is valid
+        guard let capturePhotoOutput = self.capturePhotoOutput else { return }
+        
+        // Get an instance of AVCapturePhotoSettings class
+        let photoSettings = AVCapturePhotoSettings()
+        
+        // Set photo settings for our need
+        photoSettings.isAutoStillImageStabilizationEnabled = true
+        photoSettings.isHighResolutionPhotoEnabled = true
+        photoSettings.flashMode = .off
+        
+        
+        // Call capturePhoto method by passing our photo settings and a delegate implementing AVCapturePhotoCaptureDelegate
+        capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
+    }
+    
+}
+
+
+
+extension CameraViewController : AVCapturePhotoCaptureDelegate {
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, willCapturePhotoFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+            // dispose system shutter sound
+            AudioServicesDisposeSystemSoundID(1108)
+        }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard error == nil else {
+            print("Fail to capture photo: \(String(describing: error))")
+            return
+        }
+
+        guard let imageData = photo.fileDataRepresentation() else {
+            print("Fail to convert pixel buffer")
+            return
+        }
+
+        guard let capturedImage = UIImage.init(data: imageData , scale: 1.0) else {
+            print("Fail to convert image data to UIImage")
+            return
+        }
+
+        let width = capturedImage.size.width
+        let height = capturedImage.size.height
+        let origin = CGPoint(x: (width - height)/2, y: (height - height)/2)
+        let size = CGSize(width: height, height: height)
+
+        guard let imageRef = capturedImage.cgImage?.cropping(to: CGRect(origin: origin, size: size)) else {
+            print("Fail to crop image")
+            return
+        }
+
+        let imageToSave = UIImage(cgImage: imageRef, scale: 1.0, orientation: .down)
+        UIImageWriteToSavedPhotosAlbum(imageToSave, nil, nil, nil)
+    }
+}
+
+extension CameraViewController : AVCaptureMetadataOutputObjectsDelegate {
+
+    func metadataOutput(_ captureOutput: AVCaptureMetadataOutput,
+                       didOutput metadataObjects: [AVMetadataObject],
+                       from connection: AVCaptureConnection) {
+        // Check if the metadataObjects array is contains at least one object.
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.isHidden = true
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObject.ObjectType.qr {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                messageLabel.isHidden = false
+                messageLabel.text = metadataObj.stringValue
+            }
+        }
+    }
+}
+
+extension UIInterfaceOrientation {
+    var videoOrientation: AVCaptureVideoOrientation? {
+        switch self {
+        case .portraitUpsideDown: return .portraitUpsideDown
+        case .landscapeRight: return .landscapeRight
+        case .landscapeLeft: return .landscapeLeft
+        case .portrait: return .portrait
+        default: return nil
+        }
+    }
+}
